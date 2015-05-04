a19e5e2921ce8ec737ca8a3cefc469973cd1f38f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-04 20:14:34 +0200

    Implement the view API [#6773]

--- module/view.js
+++ module/view.js
@@ -1,33 +1,67 @@
-import ø from 'stereo';
+import emit from 'stereo/emit';
+import on from 'stereo/on';
+import when from 'stereo/when';
 
-export default (rootElement, settings = {}) => {
-  const document = settings.document || document;
-
-  const elements = {};
-
-  // Scan the `rootElement` for select elements.
-  const rootChildren = Array.from(rootElement.children);
-  if (rootChildren[0].tagName === 'LABEL') elements.caption = rootChildren[0];
+const getCaption = (rootChildren) => () => {
+  if (rootChildren[0].tagName === 'LABEL') return rootChildren[0];
   else throw new Error('bare-select: ' +
     'The first element in a `<bare-select>` should be the caption – a ' +
     '`<label>` element.'
   );
+};
+
+const getSwitch = (rootChildren) => () => {
   if (
     rootChildren[1].tagName === 'INPUT' &&
     rootChildren[1].type.toLowerCase() === 'checkbox'
-  ) elements.caption = rootChildren[1];
+  ) return rootChildren[1];
   else throw new Error('bare-select: ' +
     'The second element in a `<bare-select>` should be the switch – an ' +
     '`<input type="checkbox">` element.'
   );
-  if (rootChildren[2].tagName === 'UL') elements.dropdown = rootChildren[2];
+};
+
+const getOptions = (rootChildren) => () => {
+  if (rootChildren[2].tagName === 'UL') return rootChildren[2].children;
   else throw new Error('bare-select: ' +
     'The third element in a `<bare-select>` should be the dropdown – a ' +
     '`<ul>` element.'
   );
+};
 
-  const selection = ø();
-  const captionContent = ø();
-  const options = ø();
-  const captionEvents = ø();
+export default (rootElement) => {
+
+  // Initialize internal DOM queries.
+  const rootChildren = rootElement.children;
+  const _getCaption = getCaption(rootChildren);
+  const _getSwitch = getSwitch(rootChildren);
+  const _getOptions = getOptions(rootChildren);
+
+  // Initialize input/output channels.
+  const selection = Object.freeze({
+    emit: emit(),
+  });
+
+  const captionContent = Object.freeze({
+    emit: emit(),
+  });
+
+  const emitOptions = emit();
+  const options = Object.freeze({
+    on: on(emitOptions),
+    when: when(emitOptions),
+  });
+
+  const emitCaptionElement = emit();
+  const captionElement = Object.freeze({
+    on: on(emitCaptionElement),
+  });
+
+  // Return the channels.
+  return Object.freeze({
+    selection,
+    captionContent,
+    options,
+    captionElement,
+  });
 };
