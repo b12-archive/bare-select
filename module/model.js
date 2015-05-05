13f27a9af41d410cb5e73a67ad0108d1d8f140c6
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-05 17:41:26 +0200

    Batch attribute updates [#6773]

--- module/model.js
+++ module/model.js
@@ -2,9 +2,42 @@ var øEmit = require('stereo/emit');
 var øOn = require('stereo/on');
 var øWhen = require('stereo/when');
 var asObject = require('as/object');
+var asap = require('set-immediate-shim');
+
+function attributeUpdater(args) {
+
+  // Read args.
+  var emitter = args.emitter;
+  var attributesObject = args.attributesObject;
+
+  // Make sure we emit one event for batch attribute updates.
+  var executedInThisLoop = false;
+  function resetLoop() {executedInThisLoop = false;}
+
+  return function emitUpdate() {
+    if (executedInThisLoop) return;
+    executedInThisLoop = true;
+    asap(resetLoop);
+
+    // Parse attributes and emit an object. See the specs for more info.
+    var attributesArray = Array.prototype.slice.call(attributesObject);
+    emitter(
+      attributesArray.map(function(attribute) {return attribute.name;}),
+      Object.freeze({
+        attributes: Object.freeze(
+          asObject(attributesArray.map(function (attribute) {
+            return {
+              key: attribute.name,
+              value: attribute.value,
+            };
+          }))
+        ),
+      })
+    );
+  };
+}
 
 module.exports = function model(rootElement) {
-  var rootAttributes = rootElement.attributes;
 
   // Initialize the input channel `patches`.
   var patches = Object.freeze({
@@ -17,22 +50,13 @@ module.exports = function model(rootElement) {
     on: øOn(emitUpdates),
     when: øWhen(emitUpdates),
   });
+  var emitCurrentAttributes = attributeUpdater({
+    emitter: emitUpdates,
+    attributesObject: rootElement.attributes,
+  });
 
   // Emit initial messages to `updates`.
-  var attributesArray = Array.prototype.slice.call(rootAttributes);
-  emitUpdates(
-    attributesArray.map(function(attribute) {return attribute.name;}),
-    Object.freeze({
-      attributes: Object.freeze(
-        asObject(attributesArray.map(function (attribute) {
-          return {
-            key: attribute.name,
-            value: attribute.value,
-          };
-        }))
-      ),
-    })
-  );
+  emitCurrentAttributes();
 
   return Object.freeze({
     patches: patches,
