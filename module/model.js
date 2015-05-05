25492ecbd7485cea51428a1dd1ecbc2e8963bbaf
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-05 17:06:51 +0200

    Emit initial `model.updates` event [#6773]

--- module/model.js
+++ module/model.js
@@ -1,20 +1,38 @@
 var øEmit = require('stereo/emit');
 var øOn = require('stereo/on');
 var øWhen = require('stereo/when');
-var øCatch = require('stereo/catch');
+var asObject = require('as/object');
 
 module.exports = function model(rootElement) {
+  var rootAttributes = rootElement.attributes;
+
   // Initialize the input channel `patches`.
   var patches = Object.freeze({
     emit: øEmit(),
   });
 
   // Initialize the output channel `updates`.
-  var emitOptions = øEmit();
+  var emitUpdates = øEmit();
   var updates = Object.freeze({
-    on: øOn(emitOptions),
-    when: øWhen(emitOptions),
+    on: øOn(emitUpdates),
+    when: øWhen(emitUpdates),
   });
+
+  // Emit initial messages to `updates`.
+  var attributesArray = Array.prototype.slice.call(rootAttributes);
+  emitUpdates(
+    attributesArray.map(function(attribute) {return attribute.name;}),
+    Object.freeze({
+      attributes: Object.freeze(
+        asObject(attributesArray.map(function (attribute) {
+          return {
+            key: attribute.name,
+            value: attribute.value,
+          };
+        }))
+      ),
+    })
+  );
 
   return Object.freeze({
     patches: patches,
