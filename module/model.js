0a66de22c918032de95c4a7304e6a302fa5a09f0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-05 16:14:17 +0200

    Implement the model API [#6773]

--- module/model.js
+++ module/model.js
@@ -1,1 +1,24 @@
-module.exports = function model() {};
+var øEmit = require('stereo/emit');
+var øOn = require('stereo/on');
+var øWhen = require('stereo/when');
+var øCatch = require('stereo/catch');
+
+module.exports = function model(rootElement) {
+  // Initialize the input channel `patches`.
+  var patches = Object.freeze({
+    emit: øEmit(),
+  });
+
+  // Initialize the output channel `updates`.
+  var emitOptions = øEmit();
+  var updates = Object.freeze({
+    on: øOn(emitOptions),
+    when: øWhen(emitOptions),
+    catch: øCatch(emitOptions),
+  });
+
+  return Object.freeze({
+    patches: patches,
+    updates: updates,
+  });
+};
