c7ea3b4b7c1ce7a565ac642285f91f50ca5025d8
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-05 10:54:47 +0200

    Initial `options` update [#6773]

--- module/view.js
+++ module/view.js
@@ -2,51 +2,63 @@ import emit from 'stereo/emit';
 import on from 'stereo/on';
 import when from 'stereo/when';
 import snatch from 'stereo/catch';
+import asObject from 'as/object';
 
-const getCaption = (rootChildren) => () => {
-  if (rootChildren[0].tagName === 'LABEL') return rootChildren[0];
-  else throw new Error('bare-select: ' +
-    'The first element in a `<bare-select>` should be the caption – a ' +
-    '`<label>` element.'
-  );
-};
-
-const getSwitch = (rootChildren) => () => {
-  if (
-    rootChildren[1].tagName === 'INPUT' &&
-    rootChildren[1].type.toLowerCase() === 'checkbox'
-  ) return rootChildren[1];
-  else throw new Error('bare-select: ' +
-    'The second element in a `<bare-select>` should be the switch – an ' +
-    '`<input type="checkbox">` element.'
-  );
-};
-
-const getOptions = (rootChildren) => () => {
-  if (rootChildren[2].tagName === 'UL') return rootChildren[2].children;
-  else throw new Error('bare-select: ' +
+const getOptions = (rootChildren) => {
+  const dropdown = rootChildren[2];
+  if (!dropdown || dropdown.tagName !== 'UL') return {error: {message:
+    'bare-select: ' +
     'The third element in a `<bare-select>` should be the dropdown – a ' +
     '`<ul>` element.'
-  );
+  }};
+
+  let options = Array.from(dropdown.children)
+    .filter((element) => element.tagName === 'LI')
+  ;
+
+  if (!options.length) return {error: {message: 'bare-select: ' +
+    'No options found in the dropdown. Every option should be a `<li>` ' +
+    'element with a radio button.'
+  }};
+
+  else if (options.some((item) => {
+    return (
+      !item.children[0] ||
+      item.children[0].tagName !== 'INPUT'
+    );
+  })) return {error: {message: 'bare-select: ' +
+    'The first element in every dropdown option (`<li>`) should be an ' +
+    '`<input>` element – a radio button or checkbox.'
+  }};
+
+  return {value: asObject(
+    options.map((item) => {
+      return {
+        key: item.children[0].value,
+        value: {
+          node: item,
+        },
+      };
+    })
+  )};
 };
 
 export default (rootElement) => {
 
   // Initialize internal DOM queries.
   const rootChildren = rootElement.children;
-  const _getCaption = getCaption(rootChildren);
-  const _getSwitch = getSwitch(rootChildren);
-  const _getOptions = getOptions(rootChildren);
 
-  // Initialize input/output channels.
+  // Initialize the input channel `selection`.
   const selection = Object.freeze({
     emit: emit(),
   });
 
+  // Initialize the input channel `captionContent`.
   const captionContent = Object.freeze({
     emit: emit(),
   });
 
+  // Initialize the output channel `options`.
   const emitOptions = emit();
   const options = Object.freeze({
     on: on(emitOptions),
@@ -54,6 +66,14 @@ export default (rootElement) => {
     catch: snatch(emitOptions),
   });
 
+  // Emit an initial `update` or `error` to `options`.
+  {
+    const {error, value} = getOptions(rootChildren);
+    if (error) emitOptions('error', error);
+    else emitOptions('update', value);
+  }
+
+  // Initialize the output channel `captionElement`.
   const emitCaptionElement = emit();
   const captionElement = Object.freeze({
     on: on(emitCaptionElement),
