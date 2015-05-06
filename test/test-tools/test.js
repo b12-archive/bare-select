e6461f1d55eb8507deda705f3545595f54f64a6e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-06 13:55:51 +0200

    Use Object.assign

--- test/test-tools/test.js
+++ test/test-tools/test.js
@@ -1,14 +1,12 @@
 var tape = require('tape-catch');
+var assign = require('object-assign');
 
 module.exports = function(partName) {
-  function test(title) {
-    var rest = Array.prototype.slice.apply(arguments, [1]);
-    return tape.apply(this, [partName + ':  ' + title].concat(rest));
-  }
-
-  Object.keys(tape).forEach(function(key) {
-    test[key] = tape[key];
-  });
-
-  return test;
+  return assign(
+    function test(title) {
+      var rest = Array.prototype.slice.apply(arguments, [1]);
+      return tape.apply(this, [partName + ':  ' + title].concat(rest));
+    },
+    tape
+  );
 };
