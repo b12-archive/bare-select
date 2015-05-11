27cef7b7e3daa9588c8f4f5f1d3841fcb08cf23b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-11 14:39:13 +0200

    “unfolded” plugin: specify how it updates the model [#6799]

--- test/plugins/unfolded.js
+++ test/plugins/unfolded.js
@@ -1,7 +1,16 @@
 var test = require('../test-tools/test')('“unfolded” plugin');
 var ø = require('stereo');
+var createElement = require('../test-tools/createElement');
+var h = require('virtual-dom/h');
 
 var unfolded = require('../../plugins/unfolded');
+
+function createSwitch(args) { return createElement(
+  h('input', {
+    type: 'checkbox',
+    checked: !!args.on,
+  })
+);}
 
 function mockView() { return {
   unfolded: ø(),
@@ -12,6 +21,85 @@ function mockModel() { return {
   patches: ø(),
   updates: ø(),
 }; }
+
+test(
+  'Patches the attribute `unfolded` when the switch is flicked.',
+  function(is) {
+    is.plan(4);
+
+    // Initialize the plugin.
+    var view = mockView();
+    var model = mockModel();
+    unfolded({
+      view: view,
+      model: model,
+    });
+
+    var anotherView = mockView();
+    var anotherModel = mockModel();
+    unfolded({
+      view: anotherView,
+      model: anotherModel,
+    });
+
+    // Set up tests.
+    model.patches.on('apply', function (patch) {
+      is.notOk(
+        patch.hasOwnProperty('unfolded'),
+        'removes it if the switch is off initially'
+      );
+    });
+
+    var run = 1;
+    anotherModel.patches.on('apply', function (patch) {
+      if (run === 1) {
+        is.ok(
+          patch.hasOwnProperty('unfolded'),
+          'adds it if the switch is on initially'
+        );
+      }
+
+      else if (run === 2) {
+        is.notOk(
+          patch.hasOwnProperty('unfolded'),
+          'removes it when the switch goes off'
+        );
+      }
+
+      else if (run === 3) {
+        is.ok(
+          patch.hasOwnProperty('unfolded'),
+          'adds it when the switch goes back on'
+        );
+      }
+
+      else is.fail(
+        'only applies patches when the switch changes value'
+      );
+
+      run++;
+    });
+
+    // Set fire!
+    anotherView.containerElement.emit('change', {
+      eventTarget: createSwitch({on: false})
+    });
+    view.containerElement.emit('change', {
+      eventTarget: createSwitch({on: true})
+    });
+    view.containerElement.emit('change', {
+      eventTarget: createSwitch({on: false})
+    });
+    view.containerElement.emit('change', {
+      eventTarget: createSwitch({on: false})
+    });
+    view.containerElement.emit('change', {
+      eventTarget: createSwitch({on: true})
+    });
+
+    is.end();
+  }
+);
 
 test(
   'Flicks the switch when the attribute `unfolded` has changed.',
