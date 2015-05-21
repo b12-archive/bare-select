98268e3ecbf5a1c55da2f3eb6ab7ea491237ad33
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-21 10:54:22 +0200

    Add `keyCodes` util [#6780]

---
+++ utils/keyCodes.js
@@ -0,0 +1,12 @@
+module.exports = Object.freeze({
+  TAB          : 9,
+  ENTER        : 13,
+  ESCAPE       : 27,
+  SPACE        : 32,
+  END          : 35,
+  HOME         : 36,
+  LEFT_ARROW   : 37,
+  UP_ARROW     : 38,
+  RIGHT_ARROW  : 39,
+  DOWN_ARROW   : 40,
+});
